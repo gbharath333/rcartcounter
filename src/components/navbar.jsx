a86f3a640655b52f0d79e8377054503d62e6d3c3
bharathatt <b.govardhanam@alientt.com>
2020-06-27 19:43:06 +0530

    Change text navbar to counter

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = ({ totalCounters }) => {
     return (
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
-                Navbar{" "}
-                <span className="badge badge-secondary">{totalCounters}</span>
+                Count{" "}
+                <span className="badge badge-secondary"> {totalCounters}</span>
             </a>
         </nav>
     );
